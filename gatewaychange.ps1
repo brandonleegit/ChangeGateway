2c9bc4d3aaf32acfa9fb8d12e9158e57cac66965
brandonleegit <brandon.lee@virtualizationhowto.com>
2016-09-12 14:26:39 -0500

    Update gatewaychange.ps1

--- gatewaychange.ps1
+++ gatewaychange.ps1
@@ -1,9 +1,13 @@
-﻿$index = get-netipaddress | where-object {$_.IPAddress -eq '1.2.3.4'} | select -ExpandProperty InterfaceIndex
+#variables
+$ipaddress='1.2.3.4'
+$index = get-netipaddress | where-object {$_.IPAddress -eq $ipaddress} | select -ExpandProperty InterfaceIndex
 $Log = 'c:\windows\options\gateway\gatewaychange.log'
 $gateway = get-netroute -DestinationPrefix '0.0.0.0/0' | select -ExpandProperty NextHop
 $oldroute = '1.1.1.1'
 $newroute = '2.2.2.2'
 $destination = '0.0.0.0/0'
+
+#Start Changing the Gateway if needed
 
 Function Swap-Gateway() {
 
@@ -13,7 +17,7 @@ sleep 3
 
 }
 
-if ($gateway -eq '1.2.3.4') {
+if ($gateway -eq $oldroute) {
 Write-Warning -Message "Gateway is set to $gateway and will be changed to $newroute"
 Swap-Gateway | Out-file $Log -Append
 
